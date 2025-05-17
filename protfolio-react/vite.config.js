e0bf1b12ea7990e023c65b00e95c773dc559978d
praveen2003java <praveenbe2003@gmail.com>
2025-05-17 08:41:13 +0530

    Delete protfolio-react directory

--- protfolio-react/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/protflioreact/',  // ✅ use your repo name
-  plugins: [react()],
-});
